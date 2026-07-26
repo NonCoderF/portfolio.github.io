6fe38ead7fe38f13b8759fe5b38e99f06b68c688
Jimmy <sallyinfo365@gmail.com>
2026-07-26 15:09:48 +0530

    p

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'android-command-center-v1';
-const CORE = ['./','./index.html','./assets/css/main.css','./assets/css/futuristic.css','./assets/css/os.css','./assets/js/main.js','./assets/js/particles.js','./assets/js/darkmode.js','./assets/js/typing.js','./manifest.json','./offline.html'];
+const CACHE_NAME = 'digital-nizam-seo-v2';
+const CORE = ['./','./index.html','./assets/css/main.css','./assets/css/futuristic.css','./assets/css/os.css','./assets/js/main.js','./assets/js/darkmode.js','./manifest.json','./offline.html'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./offline.html')))); });
